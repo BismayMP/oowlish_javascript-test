66daaf0c11593eef120f3a8875deb46191e42f61
bismaymp <bismaymp@gmail.com>
2020-09-11 10:43:15 -0400

    fix availabilityFilterSelect

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import ReactDOM from 'react-dom';
 import Filter from "./components/Filter";
 
 const table = document.getElementById('doctors').children
-const availableFilter = document.getElementById('availabilityFilterSelect').children
+const availableFilter = document.getElementById('availabilityFilterSelect')
 
-availableFilter.setO
+const handleAvailableFilterChange = (event, availableList) => {
+    for (let tr of table) {
+        if (event.target.value === 'available') {
+            if (!availableList.includes(tr.getAttribute('data-upin'))) 
+                tr.setAttribute('hidden', '')
+        } else {
+            tr.removeAttribute('hidden')
+        }
+    }
+}
+
+fetch('http://localhost:3030/doctors')
+    .then(response => response.json())
+    .then(response => availableFilter.onchange = (event) => handleAvailableFilterChange(event, response.filter(res => res.available).map(res => `${res.upin}`)),
+        (error => console.log(error)))
 
 
 ReactDOM.render(<Filter table={table}></Filter>, document.getElementById('searchContainer'))
-
-
-
-//const res = filterbyNameAndUpin(doctors, {name: '', upin: ''})

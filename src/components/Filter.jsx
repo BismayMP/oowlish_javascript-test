5c8d60e8955b9a4a08cc57313541872608277877
bismaymp <bismaymp@gmail.com>
2020-09-11 00:40:38 -0400

    add to name and UPIN filters onchange event for handle value changes

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 
-const Filter = () => {
+const Filter = ({ table }) => {
     const [state, setState] = useState({ name: '', UPIN: '' });
+
+    const OnFilterChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.value })
+        for (let tr of table) {
+                (`${tr.getAttribute('data-upin')}`.includes(state.UPIN) && 
+                `${tr.cells[0].textContent}`.includes(state.name))
+                ? tr.removeAttribute('hidden') : tr.setAttribute('hidden', "")
+
+        }
+    }
 
     return (
         <>
-        <input 
-        type="text" 
-        placeholder='Name' 
-        onChange={event => setState({...state, name: event.target.value})}>
-        </input>
-        <input 
-        type="text" 
-        placeholder='UPIN' 
-        onChange={event => setState({...state, UPIN: event.target.value})}>
-        </input>
+            <input
+                type="text"
+                name='name'
+                placeholder='Name'
+                onChange={OnFilterChange}>
+            </input>
+            <input
+                type="text"
+                name='UPIN'
+                placeholder='UPIN'
+                onChange={OnFilterChange}>
+            </input>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import Filter from "./components/Filter";
 
-ReactDOM.render(<Filter></Filter>, document.getElementById('searchContainer'))
+const table = document.getElementById('doctors').children
+const availableFilter = document.getElementById('availabilityFilterSelect').children
+
+availableFilter.setO
+
+
+ReactDOM.render(<Filter table={table}></Filter>, document.getElementById('searchContainer'))
+
+
+
+//const res = filterbyNameAndUpin(doctors, {name: '', upin: ''})

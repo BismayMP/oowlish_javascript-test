4dddc97930b4d80e0420f46855a2e9a2f7f89dcd
bismaymp <bismaymp@gmail.com>
2020-09-13 23:55:53 -0400

    remove test for filter component

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+const { defaults } = require('jest-config');
+module.exports = {
+  verbose: true,
+  moduleNameMapper: {
+    "\\.  (jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m  4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from 'react-dom';
-import Filter from "./components/Filter";
+import Filter from "./components/Filter.jsx";
 import { AddOnClickAndInitialStyle } from "./utils";
+
+const isDevEnv = process.env.NODE_ENV === 'development'
 
 const table = document.getElementById('doctors').children
 const availableFilter = document.getElementById('availabilityFilterSelect')
 export const doctors = []
 
-fetch('http://localhost:3030/doctors')
-    .then(response => response.json())
-    .then(response => {
-        doctors.push(...response)
+const fetchData = () => {
+    fetch('http://localhost:3030/doctors')
+        .then(response => response.json())
+        .then(response => {
+            doctors.push(...response)
 
-    },
-        (error => console.log(error)))
+        },
+            (error => console.log(error)))
+}
+
+(isDevEnv) ? fetchData() : doctors.push(
+    { "upin": 202029, "name": "John Doe", "available": false },
+    { "upin": 402910, "name": "Nick Ramsen", "available": true },
+    { "upin": 910291, "name": "Liz Redfield", "available": true },
+    { "upin": 202914, "name": "Javier Garcia", "available": false },
+    { "upin": 394840, "name": "Harry Bone", "available": true },
+    { "upin": 982170, "name": "Kevin Lamkin", "available": true },
+    { "upin": 393920, "name": "Andrew Stuart", "available": true },
+    { "upin": 655942, "name": "Maggie Willians", "available": true }
+)
 
 AddOnClickAndInitialStyle(table, doctors)
 

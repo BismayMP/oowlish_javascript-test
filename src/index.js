4f90cf3675526364eaf595e4ed67cffa9f20232e
bismaymp <bismaymp@gmail.com>
2020-09-13 21:02:58 -0400

    fix filters hanlder

--- src/index.js
+++ src/index.js
@@ -8,22 +8,6 @@ export const table = document.getElementById('doctors').children
 export const availableFilter = document.getElementById('availabilityFilterSelect')
 export const doctors = []
 
-const handleAvailableFilterChange = (event) => {
-    availableFilterFun(event.target.value)
-}
-
-export const availableFilterFun = (value = availableFilter.value) => {
-    let availableList = doctors.filter(res => res.available).map(item => `${item.upin}`)
-    for (let tr of table) {
-        if (value === 'available') {
-            if (!availableList.includes(tr.getAttribute('data-upin')))
-                tr.setAttribute('hidden', '')
-        } else {
-            tr.removeAttribute('hidden')
-        }
-    }
-}
-
 fetch('http://localhost:3030/doctors')
     .then(response => response.json())
     .then(response => {
@@ -32,7 +16,6 @@ fetch('http://localhost:3030/doctors')
     },
         (error => console.log(error)))
 
-availableFilter.onchange = (event) => handleAvailableFilterChange(event)
 AddOnClickAndInitialStyle(table, doctors)
 
-ReactDOM.render(<Filter table={table}></Filter>, document.getElementById('searchContainer'))
+ReactDOM.render(<Filter table={table} availableFilter={availableFilter}></Filter>, document.getElementById('searchContainer'))
